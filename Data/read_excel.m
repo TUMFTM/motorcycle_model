2bf23171db6784e56eb5fe1a5a65f8ffba90fece
Francesco Passigato <passigato@ftm.mw.tum.de>
2020-07-15 17:12:16 +0200

    model

---
+++ Data/read_excel.m
@@ -0,0 +1,134 @@
+[~,g] = xlsread('Data.xlsx','Geometry');
+[~,m] = xlsread('Data.xlsx','Mass-Inertia');
+[~,t] = xlsread('Data.xlsx','Tire');
+[~,s] = xlsread('Data.xlsx','Stiffness');
+filename = 'Vorlage.xml';
+DOMnode = xmlread('Vorlage.xml');
+Parameter = DOMnode.getDocumentElement;
+
+for i=1:length(g)
+    read = strcat('A',mat2str(i),':E',mat2str(i));
+    [data,name] = xlsread('Data.xlsx','Geometry',read);
+    if isempty(data) && i<4 && length(name)==1
+        file_name=name;
+    elseif isempty(data) && i>4 && length(name)==1
+        xmlwrite(strcat(char(file_name),'.xml'),DOMnode);
+        DOMnode = xmlread('Vorlage.xml');
+        Parameter = DOMnode.getDocumentElement;
+        file_name=name;
+    elseif isempty(data) && length(name)>1
+        trash = 0;
+    elseif isempty(data)==0
+        vectorPar = DOMnode.createElement('vectorParameter');
+        vectorPar.setAttribute('name',char(name));
+        xmlVec = DOMnode.createElement('xmlVector');
+        for j=1:length(data)
+            ele = DOMnode.createElement('ele');
+            ele.appendChild(DOMnode.createTextNode(sprintf('%f',data(j))));
+            xmlVec.appendChild(ele);
+        end
+        vectorPar.appendChild(xmlVec);
+        Parameter.appendChild(vectorPar);
+    else
+        trash=0;        
+    end
+end
+xmlwrite(strcat(char(file_name),'.xml'),DOMnode);
+
+for i=1:length(m)
+    read = strcat('A',mat2str(i),':K',mat2str(i));
+    [data,name] = xlsread('Data.xlsx','Mass-Inertia',read);
+    if isempty(data) && i<4 && length(name)==1
+        file_name=name;
+        DOMnode = xmlread(strcat(char(file_name),'.xml'));
+        Parameter = DOMnode.getDocumentElement;
+    elseif isempty(data) && i>4 && length(name)==1
+        xmlwrite(strcat(char(file_name),'.xml'),DOMnode);
+        file_name=name;
+        DOMnode = xmlread(strcat(char(file_name),'.xml'));
+        Parameter = DOMnode.getDocumentElement;
+    elseif isempty(data) && length(name)>1
+        trash = 0;        
+    elseif isempty(data)==0 && isempty(name)==0
+        if length(data)==1
+            scalarPar = DOMnode.createElement('scalarParameter');
+            scalarPar.setAttribute('name',char(name));
+            scalarPar.appendChild(DOMnode.createTextNode(sprintf('%f',data(1))));
+            Parameter.appendChild(scalarPar);
+        else
+            matrixPar = DOMnode.createElement('matrixParameter');
+            matrixPar.setAttribute('name',char(name));
+            xmlMat = DOMnode.createElement('xmlMatrix');
+            for j=[0,3,6]
+                row = DOMnode.createElement('row');
+                for k=1:3
+                    ele = DOMnode.createElement('ele');
+                    ele.appendChild(DOMnode.createTextNode(sprintf('%f',data(j+k))));
+                    row.appendChild(ele);
+                end
+                xmlMat.appendChild(row);
+            end
+            matrixPar.appendChild(xmlMat);
+            Parameter.appendChild(matrixPar);           
+        end
+    else
+        trash=0;
+    end
+end
+xmlwrite(strcat(char(file_name),'.xml'),DOMnode);
+
+for i=1:length(t)
+    read = strcat('A',mat2str(i),':B',mat2str(i));
+    [data,name] = xlsread('Data.xlsx','Tire',read);
+    if isempty(data) && i<4 && length(name)==1
+        file_name=name;
+        DOMnode = xmlread(strcat(char(file_name),'.xml'));
+        Parameter = DOMnode.getDocumentElement;
+    elseif isempty(data) && i>4 && length(name)==1
+        xmlwrite(strcat(char(file_name),'.xml'),DOMnode);
+        file_name=name;
+        DOMnode = xmlread(strcat(char(file_name),'.xml'));
+        Parameter = DOMnode.getDocumentElement;
+    elseif isempty(data) && length(name)>1
+        trash = 0;        
+    elseif isempty(data)==0
+        scalarPar = DOMnode.createElement('scalarParameter');
+        scalarPar.setAttribute('name',char(name));
+        scalarPar.appendChild(DOMnode.createTextNode(sprintf('%f',data(1))));
+        Parameter.appendChild(scalarPar);
+    else
+        trash=0;
+    end
+end
+xmlwrite(strcat(char(file_name),'.xml'),DOMnode);
+
+for i=1:length(s)
+    read = strcat('A',mat2str(i),':C',mat2str(i));
+    [data,name] = xlsread('Data.xlsx','Stiffness',read);
+    if isempty(data) && i<4 && length(name)==1
+        file_name=name;
+        DOMnode = xmlread(strcat(char(file_name),'.xml'));
+        Parameter = DOMnode.getDocumentElement;
+    elseif isempty(data) && i>4 && length(name)==1
+        xmlwrite(strcat(char(file_name),'.xml'),DOMnode);
+        file_name=name;
+        DOMnode = xmlread(strcat(char(file_name),'.xml'));
+        Parameter = DOMnode.getDocumentElement;
+    elseif isempty(data) && length(name)>1
+        trash = 0;        
+    elseif isempty(data)==0
+        vectorPar = DOMnode.createElement('vectorParameter');
+        vectorPar.setAttribute('name',char(name));
+        xmlVec = DOMnode.createElement('xmlVector');
+        for j=1:length(data)
+            ele = DOMnode.createElement('ele');
+            ele.appendChild(DOMnode.createTextNode(sprintf('%f',data(j))));
+            xmlVec.appendChild(ele);
+        end
+        vectorPar.appendChild(xmlVec);
+        Parameter.appendChild(vectorPar);
+    else
+        trash=0;        
+    end
+end
+xmlwrite(strcat(char(file_name),'.xml'),DOMnode);
--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-motorcycle_model
+# MBSim
+The motorcycle model is based on the open source mulitbody simulation software MBSim. 
+Please download the software under following link: https://www.mbsim-env.de/builds/run/current/win64-dailyrelease/. 
+
+# Motorcycle model
+In the following some information about the model functionalities are given.
+
+### Switch for Dofs
+The parameters with the name beginning in "enable_" allow to decide which Dofs are 
+modelled. Set the parameter to 1 to activate the Dof, to 0 to deactivate it. 
+
+### Contollers
+There are two controllers: one for the longitudinal and one for the lateral dynamics. 
+They are present in the group: `Group_Controller`. This group is then spilt in lateral
+and longitudinal controller. The parameters relative to each controller are specified 
+in the parameter list definied within the controller itself. Only the switches `Steering_controller`
+and `Velocity_controller` are in the general parameter list. 
+* Lateral dynamics controller: PD controller which allows the motorcycle to reach 
+a speficied roll angle. The desired roll angle can be given with the parameter `Setval_roll_angle`.
+The controller receives the signals of actual roll angle and roll rotation speed and 
+applies a steer moment. 
+* Longitudinal dynamics controller: PID controller which allows to perform a acceleration-braking
+maneuver. It acts with a drive moment on the rear wheel or with a brake moment on both wheels according to
+the `Brake_distribution`. The desired speed evolution can be specified with the SignalOperation `vx_target`. 
+In the present version this parameter is set in order for the motorcycle to reach a desired `v_end` 
+from the inital `v0` speed describing a Cosinus function. 
+
+### Excitations
+The motorcycle stability can be tested with some excitations:
+* `Torque_steer`: applies a cosinus shaped steer moment starting at `T_To_start`
+and ending at `T_To_exc`.
+* `Fy_Impuls`: applies a cosinus shaped lateral force to the frame starting at `T_To_start`
+and ending at `T_To_exc`.
+* Road weaves can be simulated by setting the following parameters: `h_road_weaves` for the height,
+`l_road_weaves` for their length and `n_road_weaves`to specify the number.
+
+# Motorcycle parameters
+The only motorcycle parameters which can be directly changed in MBSim are `steering_damper`
+and `caster_angle`. The other parameters, including geometry, mass-inertia, tyres and stiffnesses-damping
+are present in the Excel file in the folder `Data`. In order to change one ore more parameters please execute the following steps:
+* Modify the desired parameters in Excel and save the file.
+* Execute the script `read_excel`. This generates five .xml files, which are read from the model.
+* If the model was open, reload it.
+
+# Troubleshooting (Windows)
+The script `read_excel` is "ready to use" in Matlab. Alternatively it can be run
+in the open source "Octave". In this case, the Xerces Java 2 package must be downloaded 
+from https://xerces.apache.org/mirrors.cgi#binary. Then the following lines must be added
+at the beginning of the script:<br />
+`pkg load io`<br />
+`javaaddpath ("/path/to/xerces-2_12_1/xercesImpl.jar");`<br />
+`javaaddpath ("/path/to/xerces-2_12_1/xml-apis.jar");`<br />
+Some errors have been encountered when executing these lines. A possible one is: `error: javaMethod: 
+could not find library or dependencies: C:\Program Files\Java\OpenJDK_11.0.2\bin/jvm.dll`.
+In this case the solution was to change the system varible `JAVA_HOME` so that it 
+points to the folder containing the file `jvm.dll`.<br />
+Another error source could result if Octave is not able to find Java (JRE). In this case
+please make sure to actually have Java. If the error still remains a solution could 
+to write the following line after `pkg load io`:<br />
+`pkg load windows`<br />
+
+# Authors
+The multibody model was developed by Francesco Passigato (Chair of Automotive Technology at TU Munich) and
+Dipl.-Ing. Dirk Wisselmann. A part of the rider model was created by Michael Härtl (Master Degree Student at TU Munich).
+A great contribution to the model was given by Prof. Martin Förg (University of Landshut), who helped to 
+solve several problems and provided lots of suggestions on how to structure the model istelf. 
+
+# References 
+*  The model parameters are taken from: <br />
+R. S. Sharp, S. A. Evangelou, and D. J. N. Limebeer, <br />
+"Advances in the modelling of motorcycle dynamics",<br />
+Multibody System Dynamics, vol. 12, no. 3, pp. 251-283, 2004.<br />
